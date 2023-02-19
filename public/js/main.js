cf5169106b3e7cfa97f5f372052428f73676ea86
Fateh Ali <fatehalive@gmail.com>
2023-02-19 18:28:22 +0700

    feat: slicing assets from png

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// Add Public Static Script Here
+(function () {
+    const greeting = "Hi there! 👋"
+    console.log(
+    `${greeting}
+While you're here, how about popping over to domain/about/careers?
+We're hiring for passionate, talented engineers, and would love to hear from you! ✨`
+    )
+})()
