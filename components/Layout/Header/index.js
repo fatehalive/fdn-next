f0e695719e7f42c6e22c0765a4486ba9d3599a69
Fateh Ali <fatehalive@gmail.com>
2023-02-21 01:33:31 +0700

    refactor: enhance header component

--- components/Layout/Header/index.js
+++ components/Layout/Header/index.js
@@ -1,16 +1,207 @@
+import { useState } from "react";
 import Link from "next/link";
+import { 
+    AppBar, 
+    Box, 
+    Toolbar, 
+    IconButton, 
+    Typography,
+    InputBase,
+    Badge,
+    MenuItem,
+    Menu, 
+} from "@mui/material";
+import { 
+    Menu as MenuIcon, 
+    Search as SearchIcon, 
+    AccountCircle as AccountCircle,
+    Mail as MailIcon,
+    Notifications as NotificationsIcon,
+    MoreVert as MoreIcon 
+} from "@mui/icons-material";
 
 export default function Header() {
-    return (
-        <header style={{ border: '1px solid black' }}>
-            <button>iconmenu</button>
-            <Link href="/"><img src="/icons/logo-fd.svg" alt="logo-brand" /></Link>
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
 
-            <div>
-                <input type="search"></input>
-            </div>
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
+
+    const handleMobileMenuOpen = (event) => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
+
+    const menuId = 'primary-search-account-menu';
+    const renderMenu = (
+        <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+        >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        </Menu>
+    );
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+        <Menu id={mobileMenuId}
+          anchorEl={mobileMoreAnchorEl}
+          open={isMobileMenuOpen}
+          onClose={handleMobileMenuClose}
+          keepMounted
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <MenuItem>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="error">
+                <MailIcon />
+            </Badge>
+            </IconButton>
+            <p>Messages</p>
+          </MenuItem>
+          <MenuItem>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+              </IconButton>
+              <p>Notifications</p>
+          </MenuItem>
+          <MenuItem onClick={handleProfileMenuOpen}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="primary-search-account-menu"
+              aria-haspopup="true"
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <p>Profile</p>
+          </MenuItem>
+        </Menu>
+    );
+    
+    return (
+        // <header style={{ border: '1px solid black' }}>
             
-            <button>Login/Signup</button>
-        </header>
+        //     <button>iconmenu</button>
+        //     <Link href="/"><img src="/icons/logo-fd.svg" alt="logo-brand" /></Link>
+
+        //     <div>
+        //         <input type="search"></input>
+        //     </div>
+            
+        //     <button>Login/Signup</button>
+        // </header>
+      <Box component={"header"} sx={{ flexGrow: 2 }}>
+        <AppBar component={"div"} position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              MUI
+            </Typography>
+            {/* <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search> */}
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                <Badge badgeContent={4} color="error">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Box>
+            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="show more"
+                aria-controls={mobileMenuId}
+                aria-haspopup="true"
+                onClick={handleMobileMenuOpen}
+                color="inherit"
+              >
+                <MoreIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
+    {renderMobileMenu}
+    {renderMenu}
+      </Box>
     )
 };
