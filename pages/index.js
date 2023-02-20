76a33a8e9f191284d3274dc9ba5831596cc1aae0
Fateh Ali <fatehalive@gmail.com>
2023-02-21 01:29:06 +0700

    feat: implement app reducer in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useDidMount } from "hooks/useDidMount";
 import Layout from "@/components/Layout";
+import { changePageName } from "@/redux/actions/app.reducer";
+import { whichSideRender } from "@/helpers";
+import { counter } from "@/utils";
 
+/* -------------------- CONFIG/CONSTANTS -------------------- */
+const counterId = counter();
 const META = {
     title: "Home",
     description: "Hey SpiderMonkey, crawl me as homepage",
     favicon: {
         src: "/favicon.ico"
     }
-}
+};
 
+/* -------------------- HOMEPAGE START -------------------- */
 export default function Home(props) {
+    /* Hooks_Start */
+    const dispatch = useDispatch();
+    const didMount = useDidMount();
     
+    useEffect(() => {
+        console.log("WHICH SIDE: ", whichSide)
+        console.log("DIDMOUNT: ", didMount)
+        if (whichSide === "SERVER_SIDE") dispatch(changePageName(props.meta.title))
+        // if (didMount) dispatch()
+    }, [])
+    /* Hooks_End */
+
+    /* Event_Handlers_Start */
+    /**
+     * contain function handlers with prefix "handle".
+     * naming tips: handle<event><keterangan>
+     */
+
+    /* Event_Handlers_End */
+
+    /* Helpers_Start */
+    /**
+     * contain helper implementation
+     * or misc
+     */
+    let whichSide = whichSideRender(counterId);
+    
+    /* Helpers_End */
+    
+    /* View */
     return (
         <Layout meta={props.meta} >
             <div id="ads-space">
@@ -55,6 +93,7 @@ export default function Home(props) {
     )
 };
 
+/* -------------------- NEXT SSG -------------------- */
 export async function getStaticProps() {
     const res = await fetch(`${process.env.NEXT_APP_API_BASE_URL}/wp`);
     const wp = await res.json();
