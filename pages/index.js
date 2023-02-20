0887b97c44f7850f6bed276ff5b1baaa3f2943d5
Fateh Ali <fatehalive@gmail.com>
2023-02-20 22:19:17 +0700

    feat: layouting homepage

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,60 @@ const META = {
     }
 }
 
-export default function Home() {
+export default function Home(props) {
+    
     return (
-        <Layout meta={META} >
-            <div>HOMEPAGE CONTENT</div>
+        <Layout meta={props.meta} >
+            <div id="ads-space">
+                HOMEPAGE CONTENT
+            </div>
+            
+            <section id="editor-choice">
+
+            </section>
+            
+            <section id="match-products">
+
+            </section>
+            
+            <section id="campaign-internal">
+
+            </section>
+            
+            <section id="articles-latest">
+
+            </section>
+            
+            <section id="reviews-latest">
+
+            </section>
+            
+            <section id="groups-popular">
+
+            </section>
+            
+            <section id="videos-latest">
+
+            </section>
+            
+            <section id="products-latest-trend">
+
+            </section>
+            
+            <section id="brands-top-n">
+
+            </section>
         </Layout>
     )
 };
+
+export async function getStaticProps() {
+    const res = await fetch(`${process.env.NEXT_APP_API_BASE_URL}/wp`);
+    const wp = await res.json();
+    return {
+        props: {
+            meta: META,
+            fdnWP: wp
+        }
+    }
+}
