74ff1800bd7a1eab4a58f9419a2bc5e5fac58ce1
Fateh Ali <fatehalive@gmail.com>
2023-02-21 11:31:02 +0700

    feat: add theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
 import {store, wrapper } from "@/redux";
+import { theme } from "@/utils/theme";
 
 function App({ Component, pageProps }) {
     return (
       <Provider store={store}>
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
     )
   }
